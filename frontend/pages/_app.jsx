88449b1b9a07e125b479a42b3dfe6abee77b40af
celesia91-cpu <celesia91@gmail.com>
2025-09-14 09:23:21 -0400

    up

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,5 +1,6 @@
 import { AppStateProvider } from '../context/AppStateContext.jsx';
 import '../styles/globals.css';
+import '../styles/styles.css';
 
 export default function MyApp({ Component, pageProps }) {
   return (
